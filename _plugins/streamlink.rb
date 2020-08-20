6378c2594755844e0443db389125eb27c760e1b9
M. J. Fromberger <michael.j.fromberger@gmail.com>
2020-08-20 16:59:35 -0700

    Add a plugin to generate stream redirections.
    This will redirect each episode/n path to the corresponding YouTube link.

---
+++ _plugins/streamlink.rb
@@ -0,0 +1,39 @@
+module Jekyll
+  class Redirects < Generator
+    safe true
+    priority :low
+
+    # Only generate redirections if enabled in the main config.
+    def generate(site)
+      generate_redirects(site) if (site.config['stream_redirect'])
+    end
+
+    # Find all entries with a stream link and create a new page to redirect to
+    # the stream URL for the corresponding episode.
+    def generate_redirects(site)
+      site.data['episodes']['episodes'].each do |ep|
+        src = 'episode/%s' % ep['episode']
+        tgt = ep['youtube']
+        redirect = RedirectPage.new(site, site.source, src, tgt)
+        redirect.render(site.layouts, site.site_payload)
+        redirect.write(site.dest)
+        site.pages << redirect
+      end
+    end
+  end
+
+  # A Page that injects the specified redirects.
+  class RedirectPage < Page
+    def initialize(site, base, path, destination)
+      @site = site
+      @base = base
+      @dir  = path
+      @name = 'index.html'
+      self.process(@name)
+
+      # Read the YAML data from the layout page.
+      self.read_yaml(File.join(base, '_layouts'), 'redirect.html')
+      self.data['source_url'] = destination
+    end
+  end
+end
