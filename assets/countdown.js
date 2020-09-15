9296ae5d0950cc2503d28ee6b010b21aa9f29c9c
M. J. Fromberger <michael.j.fromberger@gmail.com>
2020-09-15 13:31:51 -0700

    Look back within 20 minutes to buffer late ending.

--- assets/countdown.js
+++ assets/countdown.js
@@ -3,15 +3,21 @@
         return count == 1 ? base : base+"s";
     }
 
+    const oneMin = 60*1000;
+    const oneHour = 60*oneMin;
+    const oneDay = 24*oneHour;
+
     function episodeStatus() {
         var now = new Date();
         var nextStart = Date.UTC(now.getFullYear(), now.getMonth(), now.getDate(), 21);
-        var nextEnd = nextStart + 3600000;
+        var nextEnd = nextStart + oneHour;
         var nowTime = now.getTime();
         if (nowTime > nextEnd) {
-            var day = 24 * 3600000;
-            nextStart += day;
-            nextEnd += day;
+            if (nowTime < nextEnd + 20*oneMin) {
+                return "The latest episode just finished streaming.";
+            }
+            nextStart += oneDay;
+            nextEnd += oneDay;
         } else if (nowTime > nextStart) {
             return "The current episode is streaming live.";
         }
