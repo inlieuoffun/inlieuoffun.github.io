3c05ad37a2c89cddd11c7eead4baae6dfb764697
M. J. Fromberger <michael.j.fromberger@gmail.com>
2020-09-16 10:58:08 -0700

    Add links to the countdown timer.
    Point to the stream while the show is running.
    Point to the latest episode when the show has recently ended.
    Declutter the formatting of the timer again.

--- assets/countdown.js
+++ assets/countdown.js
@@ -9,39 +9,43 @@
 
     function episodeStatus() {
         // For testing:
-        //var now = new Date("2020-09-19T20:53:11Z")
+        //var now = new Date("2020-09-19T21:00:00Z")
         var now = new Date();
         var nextStart = Date.UTC(now.getFullYear(), now.getMonth(), now.getDate(), 21);
         var nextEnd = nextStart + oneHour;
         var nowTime = now.getTime();
         if (nowTime > nextEnd) {
             if (nowTime < nextEnd + 15*oneMinute) {
-                return "The latest episode just finished streaming.";
+                return '🕒 The <a href="/episode/latest">latest episode</a> just finished streaming.';
             }
             nextStart += oneDay;
             nextEnd += oneDay;
         } else if (nowTime > nextStart) {
-            return "The current episode is streaming live.";
+            return 'The <a href="/stream/latest">current episode</a> is streaming live. 👀';
         }
 
         var timeLeft = (nextStart - nowTime) / oneHour;
         var hrs = Math.floor(timeLeft);
         var min = Math.floor((timeLeft - hrs) * 60);
         var tag = hrs == 0 ? "🔜 " : "";
-        var howLong =
-            (hrs > 0 ?
-             ` ${hrs} ${unit(hrs, "hour")}` : "") +
-            (min > 0 ?
-             ` ${min} ${unit(min, "minute")}.` :
-             (hrs > 0 ? "." : " just a moment!"));
-        return tag + "The next show goes live in" + howLong;
+
+        var howLong = [];
+        if (hrs > 0) {
+            howLong.push(`${hrs} ${unit(hrs, "hour")}`);
+        }
+        if (min > 0) {
+            howLong.push(`${min} ${unit(min, "minute")}`);
+        } else if (hrs == 0) {
+            howLong.push("just a moment");
+        }
+        return tag + "The next show goes live in " + howLong.join(", ") + ".";
     }
 
     var status = document.getElementById("countdown");
     if (status) {
-        status.innerText = episodeStatus();
+        status.innerHTML = episodeStatus();
         setInterval(function() {
-            status.innerText = episodeStatus();
+            status.innerHTML = episodeStatus();
         }, oneMinute);
     }
 })()
