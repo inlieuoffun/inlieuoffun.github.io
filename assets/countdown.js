04551f672c3c46232ad4d8534e4afb2b592b2e56
M. J. Fromberger <michael.j.fromberger@gmail.com>
2020-09-15 17:58:04 -0700

    Update countdown.js.
    - Fix message formatting.
    - Clean up constants.

--- assets/countdown.js
+++ assets/countdown.js
@@ -3,17 +3,19 @@
         return count == 1 ? base : base+"s";
     }
 
-    const oneMin = 60*1000;
-    const oneHour = 60*oneMin;
+    const oneMinute = 60*1000;
+    const oneHour = 60*oneMinute;
     const oneDay = 24*oneHour;
 
     function episodeStatus() {
+        // For testing:
+        //var now = new Date("2020-09-19T20:53:11Z")
         var now = new Date();
         var nextStart = Date.UTC(now.getFullYear(), now.getMonth(), now.getDate(), 21);
         var nextEnd = nextStart + oneHour;
         var nowTime = now.getTime();
         if (nowTime > nextEnd) {
-            if (nowTime < nextEnd + 20*oneMin) {
+            if (nowTime < nextEnd + 15*oneMinute) {
                 return "The latest episode just finished streaming.";
             }
             nextStart += oneDay;
@@ -22,16 +24,17 @@
             return "The current episode is streaming live.";
         }
 
-        var timeLeft = (nextStart - nowTime) / 3600000
+        var timeLeft = (nextStart - nowTime) / oneHour;
         var hrs = Math.floor(timeLeft);
         var min = Math.floor((timeLeft - hrs) * 60);
         var tag = hrs == 0 ? "🔜 " : "";
         var howLong =
             (hrs > 0 ?
-             `${hrs} ${unit(hrs, "hour")}, ` : "") +
+             ` ${hrs} ${unit(hrs, "hour")}` : "") +
             (min > 0 ?
-             `${min} ${unit(min, "minute")}.` : "just a moment!");
-        return tag + "The next show goes live in " + howLong;
+             ` ${min} ${unit(min, "minute")}.` :
+             (hrs > 0 ? "." : " just a moment!"));
+        return tag + "The next show goes live in" + howLong;
     }
 
     var status = document.getElementById("countdown");
@@ -39,6 +42,6 @@
         status.innerText = episodeStatus();
         setInterval(function() {
             status.innerText = episodeStatus();
-        }, 60000);
+        }, oneMinute);
     }
 })()
