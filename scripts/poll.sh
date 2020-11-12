e4c5dd29730e01f0a5ae8f1d0b2ea74eca118617
M. J. Fromberger <michael.j.fromberger@gmail.com>
2020-11-12 14:08:32 -0800

    Tweak poll.sh.

--- scripts/poll.sh
+++ scripts/poll.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 epdate -poll -edit |& \
   tea 'Found (\d+) updates on twitter' \
-      postnote -audible '$1 episode update is ready for editing'
+      postnote -audible 'Update ready for editing ($1)'
