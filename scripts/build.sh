bb6bfaf87fb2cfb8d730cfc83250dca62f90eb21
M. J. Fromberger <michael.j.fromberger@gmail.com>
2020-12-01 09:17:49 -0800

    Fix build break.
    I accidentally glitched the header comment which made the YAML parser think it
    had a list of two objects rather than a single list.

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
 set -o pipefail
-bundle exec jekyll serve --host 0.0.0.0 |
+bundle exec jekyll serve --host 0.0.0.0 "$@" |
     tea -- '\bdone in (\d+\.\d)\d* seconds' \
         postnote -audible 'Build complete after $1 seconds'
