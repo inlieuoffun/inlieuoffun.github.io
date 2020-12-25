41d30ae31b5e3840186fa1ad38331d44e0229f2f
M. J. Fromberger <michael.j.fromberger@gmail.com>
2020-12-24 16:16:17 -0800

    build.sh: Verify tools and deps are installed.

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -e
 set -o pipefail
+bundle install --quiet # ensure dependences and tools
 bundle exec jekyll serve --host 0.0.0.0 "$@" |
     tea -- '\bdone in (\d+\.\d)\d* seconds' \
         postnote -audible 'Build complete after $1 seconds'
