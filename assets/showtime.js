e36278eebedc198fc06651ab9e9f2c5242fcec5c
M. J. Fromberger <michael.j.fromberger@gmail.com>
2022-02-16 19:14:21 -0800

    Restore normal show time.

--- assets/showtime.js
+++ assets/showtime.js
@@ -42,7 +42,7 @@
         // If you need to set a non-standard show start time, edit showTimeUTC.
 
         var today     = todayUTC();
-        var nextStart = todayShowTime(today) + oneHour;
+        var nextStart = todayShowTime(today);
         var nextEnd   = nextStart + oneHour;
 
         if (today.now > nextEnd) {
