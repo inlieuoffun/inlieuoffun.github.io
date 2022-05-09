b5adeb1eb0b4c3e7c8b94119e1df9d7f17062885
M. J. Fromberger <michael.j.fromberger@gmail.com>
2022-05-09 15:37:04 -0700

    Update missing checker to use the latest episode.

--- scripts/missing.sh
+++ scripts/missing.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 set -euo pipefail
 
-seq 1 631 | while read -r num ; do
+latest="$(curl -s https://inlieuof.fun/latest.json | jq -r .latest.episode)"
+seq 1 "$latest" | while read -r num ; do
     q="$(printf 'transcript-%04d.json' $num)"
     if ! [[ -f "_transcripts/$q" ]] ; then
         echo $q
