5fcc3b100701af506d93f9f65aa6b88a7a0f3f97
M. J. Fromberger <michael.j.fromberger@gmail.com>
2022-05-16 20:31:33 -0700

    Format string hacking.

--- scripts/missing.sh
+++ scripts/missing.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 curl -s https://inlieuof.fun/episodes.json | jq -r '.episodes[].episode' | while read -r ep ; do
-    fn="$(printf '_transcripts/transcript-%04s.json' $ep)"
+    fn="$(printf '_transcripts/transcript-0%03s.json' $ep)"
     if [[ ! -f "$fn" ]] ; then
         echo "$ep"
     fi
