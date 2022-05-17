1e25446ea025ab51e682c959c67ed15784d8b01c
M. J. Fromberger <michael.j.fromberger@gmail.com>
2022-05-17 08:59:26 -0700

    Don't report episodes with no YT video as missing.

--- scripts/missing.sh
+++ scripts/missing.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 set -euo pipefail
 
-curl -s https://inlieuof.fun/episodes.json | jq -r '.episodes[].episode' | while read -r ep ; do
+curl -s https://inlieuof.fun/episodes.json | \
+    jq -r '.episodes[]|select(.youTubeURL).episode' | while read -r ep ; do
     fn="$(printf '_transcripts/transcript-0%03s.json' $ep)"
     if [[ ! -f "$fn" ]] ; then
         echo "$ep"
